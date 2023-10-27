6494416e39dc12dd12f5a2a284c13569cf5be7a7
Luan Lourenço <luan.tavares.lourenco@gmail.com>
2023-10-27 20:21:21 -0300

    v1.0.3

---
+++ src/PublicMethods/Interfaces/StaticPublicMethodsListableInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LTL\ListMethods\PublicMethods\Interfaces;
+
+interface StaticPublicMethodsListableInterface
+{
+    public function getMethods(): array;
+}
---
+++ src/PublicMethods/Traits/StaticPublicMethodsListable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LTL\ListMethods\PublicMethods\Traits;
+
+use LTL\ListMethods\PublicMethods\PublicMethods;
+
+trait StaticPublicMethodsListable
+{
+    public static function getMethods(): array
+    {
+        return PublicMethods::list(self::class);
+    }
+}
